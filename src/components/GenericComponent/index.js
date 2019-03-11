b904621c28fd754ce2f1a948b46f713bc6a36a8a
Quinn Elwood <ashesofowls@gmail.com>
2019-03-10 19:05:21 -0700

    Update GenericComponent

--- src/components/GenericComponent/index.js
+++ src/components/GenericComponent/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import classnames from 'classnames';
+import { string } from 'prop-types';
 
 import './style.css';
 
 const GenericComponent = (props) => {
+  const { prop } = props;
+
   return (
     <div className={classnames('style')}>
-      Generic Component
+      Generic Component { prop }
     </div>
   );
+};
+
+GenericComponent.defaultProps = {
+  prop: 'property',
+};
+
+GenericComponent.propTypes = {
+  prop: string,
 };
 
 export default GenericComponent;
