4509a98ab61e208d5068c54f81abdf3430f6144e
Quinn Elwood <ashesofowls@gmail.com>
2019-03-17 16:25:07 -0700

    Add global typography styles with stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react';
 
 const req = require.context('../src/components/', true, /stories\.js$/)
 
+import '../src/components/Typography/style.scss';
 import '../src/variables.css';
 
 function loadStories() {
